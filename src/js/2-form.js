2a81b103eaa1a736ba49ccd77e1f5750b1ca5d06
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2024-12-04 15:38:04 +0100

    update form file

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,45 @@
 'use strict';
+let formData = {
+  email: '',
+  message: '',
+};
+
+// Gettin the form.feedback-form
+const form = document.querySelector('.feedback-form');
+
+// local storage key
+const localStorageKey = 'feedback-form-state';
+
+// loading the data from local storage
+formData = JSON.parse(localStorage.getItem(localStorageKey)) || {
+  email: '',
+  message: '',
+};
+form.elements.email.value = formData.email;
+form.elements.message.value = formData.message;
+
+form.addEventListener('input', evt => {
+  const fieldName = evt.target.name;
+  const fieldValue = evt.target.value;
+  formData[fieldName] = fieldValue.trim(); //saving the data in formData without the space
+  localStorage.setItem(localStorageKey, JSON.stringify(formData)); //saving the data in local storage
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault(); // preventing default reload
+
+  if (!formData.email || !formData.message) {
+    //cheking if all field are filled
+    alert('Fill please all fields'); //in not, send the allert
+    return;
+  }
+
+  console.log(formData);
+
+  // cleaning the local storage and resetting the form
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+
+  // resetting formData
+  formData = { email: '', message: '' };
+});
